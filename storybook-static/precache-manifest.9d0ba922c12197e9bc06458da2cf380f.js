eb1fd5c6b01a6abcc95a41c4489debea75a97448
rade <bajic.rade2@gmail.com>
2020-08-05 19:50:39 +0200

    feat(sve u storybook-static folderu) inicijalni commit

---
+++ storybook-static/precache-manifest.9d0ba922c12197e9bc06458da2cf380f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a2ad116dc78dd29932827457fd072d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8bd12c375c8c0ae6e7a4.bundle.js"
+  },
+  {
+    "url": "runtime~main.8bd12c375c8c0ae6e7a4.bundle.js"
+  },
+  {
+    "revision": "d7ad9260d56acd6d56f7414209ced5b4",
+    "url": "static/media/OxygenMono-Regular.d7ad9260.ttf"
+  },
+  {
+    "revision": "e5653933f4cd9c201174f041c29788ce",
+    "url": "static/media/login.e5653933.svg"
+  },
+  {
+    "revision": "541ae5f1f9867bcce113c87a7bf16d01",
+    "url": "static/media/signup.541ae5f1.svg"
+  },
+  {
+    "url": "vendors~main.8bd12c375c8c0ae6e7a4.bundle.js"
+  },
+  {
+    "url": "vendors~main.8bd12c375c8c0ae6e7a4.bundle.js.LICENSE.txt"
+  }
+]);
